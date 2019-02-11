2c09615bc951766e6f426c77da62f3ca0c8d4265
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-11 21:16:29 +0900

    マイグレーションを実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'comments/new', to: 'comments#new'
-  get 'comments/show', to: 'comment#show'
+  get 'comments/show', to: 'comments#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
