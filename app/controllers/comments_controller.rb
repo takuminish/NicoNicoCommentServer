4c73900c2215a5710c4d0bdbe6a348d23ce9e41e
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-11 22:08:04 +0900

    コメントをセーブできるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,19 @@ class CommentsController < ApplicationController
   end
 
   def create
-    
+    @comment = Comment.new(comment_params)
+    if @comment.save
 
+    else
+
+    end
   end
   
   def show
     @comment = Comment.where(is_used: false)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text,:color,:size)
   end
 end
