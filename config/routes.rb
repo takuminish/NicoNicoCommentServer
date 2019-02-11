cecc361dafc7ca1181da234957fc97faaf4d7fec
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-11 21:14:24 +0900

    ルーティングを指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/show'
+  get 'comments/new', to: 'comments#new'
+  get 'comments/show', to: 'comment#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
