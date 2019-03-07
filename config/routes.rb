bd8bb8bd4122e4252d87740c7c12f18db4ee02d8
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-03-08 02:47:05 +0900

    URLを隠す

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/', to: 'comments#new'
-  get 'comments/show', to: 'comments#show'
+  get "comments/#{ENV['URL']}", to: 'comments#show'
   post '/comments/new', to: 'comments#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
